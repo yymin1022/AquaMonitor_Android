4d4701d688d67aa7ce16f09e455304eed0a509b9
yymin1022 <yymin1022@gmail.com>
2024-08-08 16:50:10 +0900

    Update MainActivity : Added HealthConnect Permission Logic

--- app/src/main/java/com/yong/aquamonitor/MainActivity.kt
+++ app/src/main/java/com/yong/aquamonitor/MainActivity.kt
@@ -4,13 +4,27 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.PermissionController
+import androidx.health.connect.client.permission.HealthPermission
+import androidx.health.connect.client.records.HydrationRecord
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val LOG_TAG = "AquaMonitor"
+    private val PERMISSION_LIST = setOf(
+        HealthPermission.getReadPermission(HydrationRecord::class),
+        HealthPermission.getWritePermission(HydrationRecord::class)
+    )
+
     private var healthConnectClient: HealthConnectClient? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +37,29 @@ class MainActivity : AppCompatActivity() {
         }
 
         if(!isHealthConnectAvail(applicationContext)) {
+            Log.e(LOG_TAG, "Health Connect is not Available")
             finish()
         }
 
         healthConnectClient = getHealthConnectClient(applicationContext)
+
+        var permissionFlag = false
+        val requestPermissionActivityContract = PermissionController.createRequestPermissionResultContract()
+        val requestPermissions = registerForActivityResult(requestPermissionActivityContract) { granted ->
+            if (granted.containsAll(PERMISSION_LIST)) {
+                permissionFlag = true
+            } else {
+                Log.e(LOG_TAG, "Health Connect Permission is not Granted")
+            }
+        }
+
+        if(!permissionFlag) {
+            CoroutineScope(Dispatchers.IO).launch {
+                checkPermissionsAndRun(requestPermissions, healthConnectClient!!)
+            }
+        }
+
+        Log.i(LOG_TAG, "Successfully Initialized Application")
     }
 
     private fun isHealthConnectAvail(context: Context): Boolean {
@@ -52,5 +85,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun getHealthConnectClient(context: Context): HealthConnectClient {
         return HealthConnectClient.getOrCreate(context)
+    }
+
+    private suspend fun checkPermissionsAndRun(requestPermissions: ActivityResultLauncher<Set<String>>, healthConnectClient: HealthConnectClient) {
+        val granted = healthConnectClient.permissionController.getGrantedPermissions()
+        if(!granted.containsAll(PERMISSION_LIST)) {
+            requestPermissions.launch(PERMISSION_LIST)
+        }
     }
 }
