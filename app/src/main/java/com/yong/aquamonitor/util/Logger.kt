07982dc2ccb4c1b0db0f9d263226220e426925ea
yymin1022 <yymin1022@gmail.com>
2024-08-22 18:22:59 +0900

    Update LoggerUtil : Added Log Functions

--- app/src/main/java/com/yong/aquamonitor/util/Logger.kt
+++ app/src/main/java/com/yong/aquamonitor/util/Logger.kt
@@ -1,5 +1,19 @@
 package com.yong.aquamonitor.util
 
-object Logger {
+import android.util.Log
 
+object Logger {
+    private const val LOG_TAG = "AquaMonitor"
+
+    fun LogD(msg: String) {
+        Log.d(LOG_TAG, msg)
+    }
+
+    fun LogE(msg: String) {
+        Log.e(LOG_TAG, msg)
+    }
+
+    fun LogI(msg: String) {
+        Log.i(LOG_TAG, msg)
+    }
 }
