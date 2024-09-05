7303f14536f5cf2b7676c30812f73e3d41236fe0
yymin1022 <yymin1022@gmail.com>
2024-09-05 13:26:27 +0900

    Update ConnectActivity : Added BLE Scan Logic

--- app/src/main/java/com/yong/aquamonitor/activity/ConnectActivity.kt
+++ app/src/main/java/com/yong/aquamonitor/activity/ConnectActivity.kt
@@ -1,21 +1,39 @@
 package com.yong.aquamonitor.activity
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.yong.aquamonitor.R
+import com.yong.aquamonitor.adapter.BleScanRecyclerAdapter
+import com.yong.aquamonitor.util.Logger
 
-class ConnectActivity : AppCompatActivity() {
+class ConnectActivity: AppCompatActivity(), BleScanRecyclerAdapter.OnItemClickListener {
     private var btnStartScan: Button? = null
     private var btnStopScan: Button? = null
     private var progressBar: ProgressBar? = null
     private var recyclerView: RecyclerView? = null
+
+    private var bleAdapter: BluetoothAdapter? = null
+    private var bleDevices = mutableListOf<Pair<String, String>>()
+    private var bleRecyclerAdapter: BleScanRecyclerAdapter? = null
+
+    private var isBleScanning = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,11 +57,35 @@ class ConnectActivity : AppCompatActivity() {
     }
 
     private fun startScan() {
-        progressBar!!.visibility = View.VISIBLE
+        if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.BLUETOOTH_SCAN)
+            != PackageManager.PERMISSION_GRANTED) {
+            Logger.LogI("Bluetooth / Location Permission is Not Granted")
+            return
+        }
+
+        if(!isBleScanning) {
+            Logger.LogI("BLE Scan Started")
+            isBleScanning = true
+            bleAdapter!!.startDiscovery()
+
+            val intentFilter = IntentFilter()
+            intentFilter.addAction(BluetoothDevice.ACTION_FOUND)
+            registerReceiver(broadCastReceiver,intentFilter)
+
+            bleRecyclerAdapter!!.notifyItemRangeRemoved(0, bleDevices.size)
+            bleDevices.clear()
+
+            progressBar!!.visibility = View.VISIBLE
+        }
     }
 
     private fun stopScan() {
-        progressBar!!.visibility = View.INVISIBLE
+        if(isBleScanning) {
+            Logger.LogI("BLE Scan Stopped")
+            isBleScanning = false
+
+            progressBar!!.visibility = View.INVISIBLE
+        }
     }
 
     private val btnListener = View.OnClickListener { view ->
@@ -51,5 +93,41 @@ class ConnectActivity : AppCompatActivity() {
             R.id.connect_btn_start -> startScan()
             R.id.connect_btn_stop -> stopScan()
         }
+    }
+
+    private val broadCastReceiver = object : BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when(intent?.action){
+                BluetoothDevice.ACTION_FOUND -> {
+                    if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.BLUETOOTH_SCAN)
+                        != PackageManager.PERMISSION_GRANTED) {
+                        Logger.LogI("Bluetooth / Location Permission is Not Granted")
+                        return
+                    }
+
+                    val btDevice = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE, BluetoothDevice::class.java)
+                    } else {
+                        @Suppress("DEPRECATION")
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    }
+                    if(btDevice != null) {
+                        Logger.LogD("Bluetooth device discovered. (name = ${btDevice.name},address = ${btDevice.address})")
+                        if(btDevice.name != null) {
+                            bleDevices.add(Pair(btDevice.name, btDevice.address))
+                            bleRecyclerAdapter!!.notifyItemInserted(bleDevices.size)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onItemClick(device: Pair<String, String>) {
+        stopScan()
+
+        val intent = Intent(applicationContext, ConnectActivity::class.java)
+        intent.putExtra("BLE_DEVICE_ADDRESS", device.second)
+        startActivity(intent)
     }
 }
