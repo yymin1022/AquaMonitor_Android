9a2ea9b540affe2cdbd3224b5dd178609d18c5b9
yymin1022 <yymin1022@gmail.com>
2024-09-05 17:20:03 +0900

    Update BleService : Added Connection Logic

--- app/src/main/java/com/yong/aquamonitor/service/BleService.kt
+++ app/src/main/java/com/yong/aquamonitor/service/BleService.kt
@@ -1,12 +1,34 @@
 package com.yong.aquamonitor.service
 
+import android.Manifest
 import android.app.Service
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattDescriptor
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
+import androidx.core.app.ActivityCompat
 import com.yong.aquamonitor.util.Logger
+import java.util.UUID
 
 class BleService: Service() {
+    companion object {
+        const val UUID_CHARACTERISTIC = "00000000-0000-0000-0000-008000000000"
+        const val UUID_DESCRIPTOR = "00000000-0000-0000-0000-008000000000"
+        const val UUID_SERVICE = "00000000-0000-0000-0000-008000000000"
+    }
+
+    private var bleAdapter: BluetoothAdapter? = null
+    private var bleGatt: BluetoothGatt? = null
+
     private val binder = LocalBinder()
 
     inner class LocalBinder: Binder() {
@@ -25,6 +47,78 @@ class BleService: Service() {
     }
 
     override fun onBind(intent: Intent?): IBinder? {
-        return null
+        return binder
+    }
+
+    fun connectBle(deviceAddr: String): Boolean {
+        if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.BLUETOOTH_SCAN)
+            != PackageManager.PERMISSION_GRANTED) {
+            Logger.LogI("Bluetooth / Location Permission is Not Granted")
+            return false
+        }
+
+        bleAdapter = (getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
+        val bleDevice = bleAdapter!!.getRemoteDevice(deviceAddr)
+        bleGatt = bleDevice.connectGatt(this, true, gattCallback, 2)
+        return true
+    }
+
+    private val gattCallback = object: BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.BLUETOOTH_SCAN)
+                != PackageManager.PERMISSION_GRANTED) {
+                Logger.LogI("Bluetooth / Location Permission is Not Granted")
+                return
+            }
+
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                Logger.LogI("Device Connected")
+                gatt.discoverServices()
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                Logger.LogI("Device Disconnected")
+                bleGatt = null
+            }
+        }
+
+        @Suppress("DEPRECATION")
+        override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+            if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.BLUETOOTH_SCAN)
+                != PackageManager.PERMISSION_GRANTED) {
+                Logger.LogI("Bluetooth / Location Permission is Not Granted")
+                return
+            }
+
+            Logger.LogI("Discovering Service")
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                Logger.LogI("Discovered Service")
+                val service = gatt.getService(UUID.fromString(UUID_SERVICE))
+                val characteristic = service?.getCharacteristic(UUID.fromString(UUID_CHARACTERISTIC))
+                if(service != null && characteristic != null) {
+                    Logger.LogI("Finding Response Characteristic")
+                    gatt.setCharacteristicNotification(characteristic, true)
+
+                    characteristic.writeType = BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
+                    val descriptor = characteristic.getDescriptor(UUID.fromString(UUID_DESCRIPTOR))
+                    if(descriptor != null) {
+                        Logger.LogI("Enabled Notify Receive")
+
+                        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                            gatt.writeDescriptor(descriptor, BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE)
+                        } else {
+                            descriptor.value = BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
+                            gatt.writeDescriptor(descriptor)
+                        }
+                    }
+                }
+            }
+        }
+
+        @Deprecated("Deprecated in Java")
+        override fun onCharacteristicChanged(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic) {
+            @Suppress("DEPRECATION") val data = characteristic.value?.toString(Charsets.UTF_8)
+            if(data != null) {
+                Logger.LogI("Message Received: [$data]")
+            }
+        }
     }
 }
