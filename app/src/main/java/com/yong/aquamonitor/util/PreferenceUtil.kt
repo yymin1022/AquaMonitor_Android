9d109b0f9fe3b7c36b644d7e97542a6987a38dc3
yymin1022 <yymin1022@gmail.com>
2024-09-05 12:24:57 +0900

    Update PreferenceUtil : Added Functions

--- app/src/main/java/com/yong/aquamonitor/util/PreferenceUtil.kt
+++ app/src/main/java/com/yong/aquamonitor/util/PreferenceUtil.kt
@@ -1,4 +1,15 @@
 package com.yong.aquamonitor.util
 
 object PreferenceUtil {
+    private fun initPreference() {
+
+    }
+
+    fun getLastMacAddress(): String? {
+        return null
+    }
+
+    fun setMacAddress(mac: String) {
+        
+    }
 }
