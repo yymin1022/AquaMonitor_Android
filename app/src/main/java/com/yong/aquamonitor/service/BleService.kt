3349fbc19a0b586d868663eddb4248c143ca1ba2
yymin1022 <yymin1022@gmail.com>
2024-09-05 13:34:45 +0900

    Update BleService : Added Binder Inner Class

--- app/src/main/java/com/yong/aquamonitor/service/BleService.kt
+++ app/src/main/java/com/yong/aquamonitor/service/BleService.kt
@@ -2,10 +2,17 @@ package com.yong.aquamonitor.service
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import com.yong.aquamonitor.util.Logger
 
 class BleService: Service() {
+    private val binder = LocalBinder()
+
+    inner class LocalBinder: Binder() {
+        fun getService(): BleService = this@BleService
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Logger.LogD("Started")
 
