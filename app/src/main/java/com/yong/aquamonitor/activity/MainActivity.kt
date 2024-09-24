b40ca7ef5594ab867ee703588762b912d685dff5
yymin1022 <yymin1022@gmail.com>
2024-09-25 00:49:08 +0900

    Update MainActivity : Applied HealthConnectUtil:

--- app/src/main/java/com/yong/aquamonitor/activity/MainActivity.kt
+++ app/src/main/java/com/yong/aquamonitor/activity/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import android.net.Uri
 import android.os.Bundle
 import android.os.IBinder
 import android.view.View
@@ -20,27 +19,22 @@ import androidx.health.connect.client.HealthConnectClient
 import androidx.health.connect.client.PermissionController
 import androidx.health.connect.client.permission.HealthPermission
 import androidx.health.connect.client.records.HydrationRecord
-import androidx.health.connect.client.records.metadata.DataOrigin
-import androidx.health.connect.client.records.metadata.Metadata
-import androidx.health.connect.client.request.AggregateRequest
-import androidx.health.connect.client.time.TimeRangeFilter
-import androidx.health.connect.client.units.Volume
 import androidx.lifecycle.lifecycleScope
 import com.yong.aquamonitor.R
 import com.yong.aquamonitor.service.BleService
+import com.yong.aquamonitor.util.HealthConnectUtil
+import com.yong.aquamonitor.util.HealthConnectUtil.getCurrentHydration
+import com.yong.aquamonitor.util.HealthConnectUtil.isHealthConnectAvail
+import com.yong.aquamonitor.util.HealthConnectUtil.updateHydration
 import com.yong.aquamonitor.util.Logger
 import com.yong.aquamonitor.util.PreferenceUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.time.Instant
-import java.time.ZoneOffset
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
-    private var healthConnectClient: HealthConnectClient? = null
     private val healthPermissionList = setOf(
         HealthPermission.getReadPermission(HydrationRecord::class),
         HealthPermission.getWritePermission(HydrationRecord::class)
@@ -97,8 +91,6 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
-        healthConnectClient = getHealthConnectClient(applicationContext)
-
         var permissionFlag = false
         val requestPermissionActivityContract = PermissionController.createRequestPermissionResultContract()
         val requestPermissions = registerForActivityResult(requestPermissionActivityContract) { granted ->
@@ -111,12 +103,15 @@ class MainActivity : AppCompatActivity() {
 
         if(!permissionFlag) {
             CoroutineScope(Dispatchers.IO).launch {
-                checkPermissionsAndRun(requestPermissions, healthConnectClient!!)
+                checkPermissionsAndRun(requestPermissions, HealthConnectUtil.getHealthConnectClient(applicationContext))
             }
         }
 
         Logger.LogI("Successfully Initialized Application")
-        getCurrentHydration()
+        lifecycleScope.launch {
+            val hydrationValue = getCurrentHydration(applicationContext)
+            tvValue!!.text = String.format(Locale.getDefault(), "Current Hydration Value : %.2f ml", hydrationValue?: -1.0)
+        }
 
         val lastMac = getLastMac()
         if(lastMac != null) {
@@ -147,86 +142,6 @@ class MainActivity : AppCompatActivity() {
         bindService(serviceIntent, bleServiceConnection, Context.BIND_AUTO_CREATE)
     }
 
-    private fun getCurrentHydration() {
-        var hydrationValue: Double? = null
-
-        lifecycleScope.launch {
-            Logger.LogI("Reading Current Value...")
-            withContext(Dispatchers.IO) {
-                try {
-                    val hydrationResponse = async { healthConnectClient!!.aggregate(
-                        AggregateRequest(
-                            metrics = setOf(HydrationRecord.VOLUME_TOTAL),
-                            timeRangeFilter = TimeRangeFilter.before(Instant.now())
-                        )
-                    ) }.await()[HydrationRecord.VOLUME_TOTAL]
-                    hydrationValue = hydrationResponse?.inMilliliters
-                } catch(e: Exception) {
-                    Logger.LogE("Health Connect Get Error: [$e]")
-                }
-            }
-            withContext(Dispatchers.Main) {
-                tvValue!!.text = String.format(Locale.getDefault(), "Current Hydration Value : %.2f ml", hydrationValue?: -1.0)
-            }
-        }
-    }
-
-    private suspend fun updateHydration(value: Int) {
-        Logger.LogI("Updating Current Value with $value...")
-
-        try {
-            val hydrationRecord = HydrationRecord(
-                volume = Volume.milliliters(value.toDouble()),
-                startTime = Instant.now(),
-                endTime = Instant.now(),
-                startZoneOffset = ZoneOffset.MIN,
-                endZoneOffset = ZoneOffset.MIN,
-                metadata = Metadata(
-                    id = Instant.now().toString(),
-                    DataOrigin("com.yong.aquamonitor"),
-                    Instant.now())
-            )
-            val insertResult = healthConnectClient!!.insertRecords(listOf(hydrationRecord))
-            for(res in insertResult.recordIdsList) {
-                Logger.LogI("Inserted $res")
-            }
-        } catch(e: Exception) {
-            Logger.LogE("Health Connect Set Error: [$e]")
-        }
-    }
-
-    private fun isHealthConnectAvail(context: Context): Boolean {
-        val availStatus = HealthConnectClient.getSdkStatus(context)
-        if(availStatus == HealthConnectClient.SDK_UNAVAILABLE) {
-            return false
-        }
-        if(availStatus == HealthConnectClient.SDK_UNAVAILABLE_PROVIDER_UPDATE_REQUIRED) {
-            val vendingUrl = "market://details?id=com.google.android.apps.healthdata&url=healthconnect%3A%2F%2Fonboarding"
-            context.startActivity(
-                Intent(Intent.ACTION_VIEW).apply {
-                    setPackage("com.android.vending")
-                    data = Uri.parse(vendingUrl)
-                    putExtra("overlay", true)
-                    putExtra("callerId", context.packageName)
-                }
-            )
-            return false
-        }
-
-        return true
-    }
-
-    private fun getHealthConnectClient(context: Context): HealthConnectClient {
-        return HealthConnectClient.getOrCreate(context)
-    }
-
-    private suspend fun checkPermissionsAndRun(requestPermissions: ActivityResultLauncher<Set<String>>, healthConnectClient: HealthConnectClient) {
-        val granted = healthConnectClient.permissionController.getGrantedPermissions()
-        if(!granted.containsAll(healthPermissionList)) {
-            requestPermissions.launch(healthPermissionList)
-        }
-    }
-
     private val btnListener = View.OnClickListener { view ->
         when(view.id) {
             R.id.main_btn_ble_request_reset -> {
@@ -241,16 +156,24 @@ class MainActivity : AppCompatActivity() {
                 lifecycleScope.launch {
                     withContext(Dispatchers.IO) {
                         try {
-                            updateHydration(inputValue!!.text.toString().toInt())
+                            updateHydration(inputValue!!.text.toString().toInt(), applicationContext)
                         } catch(e: NumberFormatException) {
                             Logger.LogE("Failed to get Hydration Value Input")
                         }
                     }
                     withContext(Dispatchers.Main) {
-                        getCurrentHydration()
+                        val hydrationValue = getCurrentHydration(applicationContext)
+                        tvValue!!.text = String.format(Locale.getDefault(), "Current Hydration Value : %.2f ml", hydrationValue?: -1.0)
                     }
                 }
             }
+        }
+    }
+
+    private suspend fun checkPermissionsAndRun(requestPermissions: ActivityResultLauncher<Set<String>>, healthConnectClient: HealthConnectClient) {
+        val granted = healthConnectClient.permissionController.getGrantedPermissions()
+        if(!granted.containsAll(healthPermissionList)) {
+            requestPermissions.launch(healthPermissionList)
         }
     }
 }
