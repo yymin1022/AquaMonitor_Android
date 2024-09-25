991ec89efe69d89dd3a465eaf949b39d4ad8bcbb
yymin1022 <yymin1022@gmail.com>
2024-09-25 21:25:35 +0900

    Update AquaMonitorData : Added ID Variable

--- app/src/main/java/com/yong/aquamonitor/util/AquaMonitorData.kt
+++ app/src/main/java/com/yong/aquamonitor/util/AquaMonitorData.kt
@@ -2,7 +2,7 @@ package com.yong.aquamonitor.util
 
 import java.io.Serializable
 
-data class AquaMonitorData(val cycle: Int, val value: Double, val timeFrom: Long, val timeTo: Long, val type: DrinkType?): Serializable
+data class AquaMonitorData(val cycle: Int, val value: Double, val timeFrom: Long, val timeTo: Long, var type: DrinkType?, var id: String?): Serializable
 
 enum class DrinkType {
     DRINK_BEVERAGE,
