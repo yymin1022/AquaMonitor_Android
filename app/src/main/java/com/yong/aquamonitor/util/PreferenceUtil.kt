3fbb60cb04090e8fd948db243e957976500ab398
yymin1022 <yymin1022@gmail.com>
2024-09-25 21:58:21 +0900

    Update PreferenceUtil : Added HealthData Saving Logic

--- app/src/main/java/com/yong/aquamonitor/util/PreferenceUtil.kt
+++ app/src/main/java/com/yong/aquamonitor/util/PreferenceUtil.kt
@@ -2,6 +2,13 @@ package com.yong.aquamonitor.util
 
 import android.content.Context
 import android.content.SharedPreferences
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import java.io.Serializable
+import java.util.Base64
+
 
 object PreferenceUtil {
     private var pref: SharedPreferences? = null
@@ -14,11 +21,38 @@ object PreferenceUtil {
 
     fun getHealthData(id: String, context: Context): AquaMonitorData? {
         initPreference(context)
-        return null
+        val dataEncoded = pref!!.getString(id, null) ?: return null
+        try {
+            ByteArrayInputStream(Base64.getDecoder().decode(dataEncoded)).use { byteArrayInputStream ->
+                ObjectInputStream(byteArrayInputStream).use { objectInputStream ->
+                    return objectInputStream.readObject() as AquaMonitorData
+                }
+            }
+        } catch (e: java.lang.Exception) {
+            Logger.LogE("Error when saving: $e")
+            return null
+        }
     }
 
     fun saveHealthData(data: AquaMonitorData, context: Context) {
         initPreference(context)
+
+        val dataID = data.id ?: return
+        try {
+            ByteArrayOutputStream().use { byteArrayOutputStream ->
+                ObjectOutputStream(byteArrayOutputStream).use { objectOutputStream ->
+                    objectOutputStream.writeObject(data)
+                    val dataEncoded = Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray())
+
+                    val prefEd = pref!!.edit()
+                    prefEd.putString(dataID, dataEncoded)
+                    prefEd.apply()
+                }
+            }
+        } catch (e: Exception) {
+            Logger.LogE("Error when saving: $e")
+            return
+        }
     }
 
     fun getLastMacAddress(context: Context): String? {
