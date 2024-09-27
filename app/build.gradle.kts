bb86a0c93a3f132df9cb41bf1a9cf51ea626a49f
yymin1022 <yymin1022@gmail.com>
2024-09-27 22:43:33 +0900

    Update Project : Added androidx CardView Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.cardview)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
