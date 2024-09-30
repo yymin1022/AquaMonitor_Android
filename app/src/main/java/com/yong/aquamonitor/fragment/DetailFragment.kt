28ad3d214c3a271d81893856c8173123b8cc09cf
yymin1022 <yymin1022@gmail.com>
2024-10-01 05:06:40 +0900

    Update DetailFragment : Added Data Recycler View Logic

--- app/src/main/java/com/yong/aquamonitor/fragment/DetailFragment.kt
+++ app/src/main/java/com/yong/aquamonitor/fragment/DetailFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.yong.aquamonitor.R
 import com.yong.aquamonitor.adapter.DetailDataRecyclerAdapter
 import com.yong.aquamonitor.util.AquaMonitorData
+import com.yong.aquamonitor.util.HealthConnectUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class DetailFragment: Fragment(), DetailDataRecyclerAdapter.OnItemClickListener {
     private var recyclerDetailData: RecyclerView? = null
@@ -35,7 +40,13 @@ class DetailFragment: Fragment(), DetailDataRecyclerAdapter.OnItemClickListener 
     }
 
     private fun getDataList() {
-
+        lifecycleScope.launch {
+            val dataList = HealthConnectUtil.getTotalHydrationData(requireActivity())
+            dataList.forEach { data ->
+                detailDataList.add(data)
+                recyclerDetailAdapter!!.notifyItemInserted(detailDataList.size)
+            }
+        }
     }
 
     override fun onItemClick(dataItem: AquaMonitorData) {
