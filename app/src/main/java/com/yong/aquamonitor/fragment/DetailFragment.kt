0873d02afed62d7861262ad07a7a3249b83ef60c
yymin1022 <yymin1022@gmail.com>
2024-10-01 03:25:50 +0900

    Update DetailDataRecyclerAdapter : Added Recycler Initialization

--- app/src/main/java/com/yong/aquamonitor/fragment/DetailFragment.kt
+++ app/src/main/java/com/yong/aquamonitor/fragment/DetailFragment.kt
@@ -5,18 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.yong.aquamonitor.R
+import com.yong.aquamonitor.adapter.DetailDataRecyclerAdapter
+import com.yong.aquamonitor.util.AquaMonitorData
 
-class DetailFragment: Fragment() {
-    private var recyclerDetail: RecyclerView? = null
+class DetailFragment: Fragment(), DetailDataRecyclerAdapter.OnItemClickListener {
+    private var recyclerDetailData: RecyclerView? = null
+
+    private var detailDataList = mutableListOf<AquaMonitorData>()
+    private var recyclerDetailAdapter: DetailDataRecyclerAdapter? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val layoutInflater = inflater.inflate(R.layout.fragment_detail, container, false)
-        recyclerDetail = layoutInflater.findViewById(R.id.main_detail_recycler)
+
+        recyclerDetailAdapter = DetailDataRecyclerAdapter(detailDataList, this)
+        recyclerDetailData = layoutInflater.findViewById(R.id.main_detail_recycler)
+
+        recyclerDetailData!!.adapter = recyclerDetailAdapter
+        recyclerDetailData!!.layoutManager = LinearLayoutManager(requireActivity())
+
+        getDataList()
 
         return layoutInflater
+    }
+
+    private fun getDataList() {
+
+    }
+
+    override fun onItemClick(dataItem: AquaMonitorData) {
+        TODO("Not yet implemented")
     }
 }
