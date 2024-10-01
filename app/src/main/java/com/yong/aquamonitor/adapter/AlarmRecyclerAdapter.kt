5df0d1ac93b31373de362be104360b28b81864c4
yymin1022 <yymin1022@gmail.com>
2024-10-01 10:28:26 +0900

    Added AlarmRecyclerAdapter

---
+++ app/src/main/java/com/yong/aquamonitor/adapter/AlarmRecyclerAdapter.kt
@@ -0,0 +1,50 @@
+package com.yong.aquamonitor.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.yong.aquamonitor.R
+import com.yong.aquamonitor.util.AlarmData
+import com.yong.aquamonitor.util.AquaMonitorData
+import com.yong.aquamonitor.util.DrinkType
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class AlarmRecyclerAdapter(
+    private val dataList: List<AlarmData>,
+    private val itemDeleteClickListener: OnItemClickListener
+): RecyclerView.Adapter<AlarmRecyclerAdapter.ViewHolder>() {
+
+    interface OnItemClickListener {
+        fun onItemDeleteClick(position: Int)
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val icDelete: ImageButton = itemView.findViewById(R.id.recycler_item_alarm_delete)
+        val tvTime: TextView = itemView.findViewById(R.id.recycler_item_alarm_time)
+        val tvValue: TextView = itemView.findViewById(R.id.recycler_item_alarm_value)
+
+        init {
+            icDelete.setOnClickListener { view ->
+                itemDeleteClickListener.onItemDeleteClick(adapterPosition)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_detail_adapter, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val dataItem = dataList[position]
+        holder.tvTime.text = String.format(Locale.getDefault(), "%d:%d", dataItem.hour, dataItem.min)
+        holder.tvValue.text = String.format(Locale.getDefault(), "%dml", dataItem.value)
+    }
+
+    override fun getItemCount() = dataList.size
+}
