11c742e3f9ac20fff6c9431d43903ec56a6b100a
yymin1022 <yymin1022@gmail.com>
2024-10-02 06:21:29 +0900

    Update HomeFragment : Added TargetRemain Text View

--- app/src/main/java/com/yong/aquamonitor/fragment/HomeFragment.kt
+++ app/src/main/java/com/yong/aquamonitor/fragment/HomeFragment.kt
@@ -6,7 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
 import com.yong.aquamonitor.R
+import com.yong.aquamonitor.activity.MainActivity
+import com.yong.aquamonitor.util.DrinkType
+import com.yong.aquamonitor.util.HealthConnectUtil
+import com.yong.aquamonitor.util.PreferenceUtil
+import kotlinx.coroutines.launch
+import java.util.Locale
+import kotlin.math.max
 
 class HomeFragment: Fragment() {
     private var tvAlarmTime: TextView? = null
@@ -22,6 +30,27 @@ class HomeFragment: Fragment() {
         tvAlarmValue = layoutInflater.findViewById(R.id.main_home_alarm_value)
         tvTargetRemain = layoutInflater.findViewById(R.id.main_home_target_remain)
 
+        setNextAlarmView()
+        setTargetRemainView()
+
         return layoutInflater
+    }
+
+    private fun setNextAlarmView() {
+
+    }
+
+    private fun setTargetRemainView() {
+        lifecycleScope.launch {
+            val hydrationBeverage = HealthConnectUtil.getTodayHydrationByType(requireActivity(), DrinkType.DRINK_BEVERAGE) ?: 0.0f
+            val hydrationCoffee = HealthConnectUtil.getTodayHydrationByType(requireActivity(), DrinkType.DRINK_COFFEE) ?: 0.0f
+            val hydrationWater = (HealthConnectUtil.getTodayHydrationByType(requireActivity(), DrinkType.DRINK_WATER) ?: 0.0f) + (activity as MainActivity).bleService!!.aquaCurValue.toFloat()
+
+            val hydrationValue = hydrationBeverage * 0.8f + hydrationCoffee * 0.9f + hydrationWater
+            val targetValue = PreferenceUtil.getProfileTarget(requireActivity())
+
+            val remainValue = max((targetValue - hydrationValue).toInt(), 0)
+            tvTargetRemain!!.text = String.format(Locale.getDefault(), "오늘은 %dml 더 마사면 목표를 달성해요!", remainValue)
+        }
     }
 }
