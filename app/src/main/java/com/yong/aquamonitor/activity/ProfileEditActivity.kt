15053f21d75e9050c72a5f84b55ddf68c6108fb2
yymin1022 <yymin1022@gmail.com>
2024-10-02 02:03:30 +0900

    Update ProfileEdit Activity Layout : Added Layout Elements

--- app/src/main/java/com/yong/aquamonitor/activity/ProfileEditActivity.kt
+++ app/src/main/java/com/yong/aquamonitor/activity/ProfileEditActivity.kt
@@ -12,7 +12,7 @@ class ProfileEditActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile_edit)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.profile_edit)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
