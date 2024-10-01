7e5736856e8e2254cc62d09d1f8226974dbff225
yymin1022 <yymin1022@gmail.com>
2024-10-02 06:16:37 +0900

    Update HomeFragment : Added UI Element Initialization

--- app/src/main/java/com/yong/aquamonitor/fragment/HomeFragment.kt
+++ app/src/main/java/com/yong/aquamonitor/fragment/HomeFragment.kt
@@ -5,14 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.yong.aquamonitor.R
 
-class HomeFragment : Fragment() {
+class HomeFragment: Fragment() {
+    private var tvAlarmTime: TextView? = null
+    private var tvAlarmValue: TextView? = null
+    private var tvTargetRemain: TextView? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val layoutInflater = inflater.inflate(R.layout.fragment_home, container, false)
+        tvAlarmTime = layoutInflater.findViewById(R.id.main_home_alarm_time)
+        tvAlarmValue = layoutInflater.findViewById(R.id.main_home_alarm_value)
+        tvTargetRemain = layoutInflater.findViewById(R.id.main_home_target_remain)
+
         return layoutInflater
     }
 }
