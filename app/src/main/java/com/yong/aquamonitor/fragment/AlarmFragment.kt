0c13ea2cceef56e5e8d15418492596377f3b5582
yymin1022 <yymin1022@gmail.com>
2024-10-01 10:20:43 +0900

    Update Alarm Fragment : Added UI Logic

--- app/src/main/java/com/yong/aquamonitor/fragment/AlarmFragment.kt
+++ app/src/main/java/com/yong/aquamonitor/fragment/AlarmFragment.kt
@@ -5,13 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
 import com.yong.aquamonitor.R
 
 class AlarmFragment : Fragment() {
+    private var btnAdd: Button? = null
+    private var recyclerAlarmList: RecyclerView? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_alarm, container, false)
+        val layoutInflater = inflater.inflate(R.layout.fragment_alarm, container, false)
+        recyclerAlarmList = layoutInflater.findViewById(R.id.main_alarm_recycler)
+        btnAdd = layoutInflater.findViewById(R.id.main_alarm_btn_add)
+        btnAdd!!.setOnClickListener(btnListener)
+
+        return layoutInflater
+    }
+
+    private val btnListener = View.OnClickListener { view ->
+        when(view.id) {
+            R.id.main_alarm_btn_add -> {
+
+            }
+        }
     }
 }
