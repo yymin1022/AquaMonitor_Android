27386866ec36e6999a51e218da47f558907808a7
yymin1022 <yymin1022@gmail.com>
2024-10-01 10:47:42 +0900

    Update AlarmRecyclerAdapter : Fixed Typo

--- app/src/main/java/com/yong/aquamonitor/adapter/AlarmRecyclerAdapter.kt
+++ app/src/main/java/com/yong/aquamonitor/adapter/AlarmRecyclerAdapter.kt
@@ -36,7 +36,7 @@ class AlarmRecyclerAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_detail_adapter, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_alarm_adapter, parent, false)
         return ViewHolder(view)
     }
 
